d87071cf9326bfa162d47da6a31ae1de2a8eaf6b
nitro <mohanksriram@gmail.com>
2023-07-06 22:20:56 -0400

    modify buffer api

--- robobuf/buffers.py
+++ robobuf/buffers.py
@@ -1,4 +1,6 @@
-import cv2, deepcopy
+import copy
+
+import cv2
 import numpy as np
 
 
@@ -8,67 +10,79 @@ class Transition:
         self.action = action
         self.reward = reward
 
-        self.prev_trans = prev_trans
-        self.next_trans = next_trans
+        self.prev = prev_trans
+        self.next = next_trans
 
     @property
     def done(self):
-        return self.next_trans is None
+        return self.next is None
 
     @property
     def first(self):
-        return self.prev_trans is None
+        return self.prev is None
+
+    def to_tuple(self):
+        return (self.obs.to_dict(), self.action, self.reward)
 
 
 class ObsWrapper:
     def __init__(self, obs, H=256, W=256):
-        self.encoded_imgs = []
         self.H = H
         self.W = W
 
-        num_img_keys = len([k for k in obs.keys() if "image" in k])
-        for i in range(num_img_keys):
-            self.encoded_imgs.append(obs[f"image_{i}"])
+        self.obs = {k: v for k, v in obs.items() if "cam" not in k}
 
-        # delete images from obs
-        self.obs = {k: v for k, v in obs.items() if "image" not in k}
-    
+        cam_keys = [k for k in obs.keys() if "cam" in k]
+        for i, cam_key in enumerate(cam_keys):
+            if "enc" not in cam_key:
+                self.obs[f"enc_cam_{i}"] = self._encode_image(obs[cam_key])
+            else:
+                self.obs[cam_key] = obs[cam_key]
 
-    def add_image(self, image):
-        success, encoded_image = cv2.imencode('.jpg', image)
-        if success:
-            self.encoded_imgs.append(encoded_image)
-        else:
-            print("failed to encode image")
+    @property
+    def state(self):
+        return self.obs["state"]
+
+    def _encode_image(self, image):
+        _, encoded_image = cv2.imencode(".jpg", image)
+        return encoded_image
 
     def image(self, idx=0):
-        encoded_image_np = np.frombuffer(self.encoded_imgs[idx], dtype=np.uint8)
+        encoded_image_np = np.frombuffer(self.obs[f"enc_cam_{idx}"], dtype=np.uint8)
         cv2_image = cv2.imdecode(encoded_image_np, cv2.IMREAD_COLOR)
         cv2_image = cv2.resize(cv2_image, (self.H, self.W))
         rgb_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
         return rgb_image
-    
+
     def to_dict(self):
-        obs = deepcopy.copy(self.obs)
-        for i in range(len(self.encoded_imgs)):
-            obs[f"enc_image_{i}"] = self.encoded_imgs[i]
+        obs = copy.deepcopy(self.obs)
         return obs
 
     @classmethod
-    def from_dict(self):  # this is dict to ObsWrapper parser
-        raise NotImplementedError
+    def from_dict(self, obs):  # this is dict to ObsWrapper parser
+        return ObsWrapper(obs)
+
 
 class ReplayBuffer:
-    def __init__(self, max_size: int):
+    def __init__(self, max_size: int = np.inf):
         self._max_size = max_size
         self._buffer = []
-        self._traj_starts = [] # first observation in each traj
+        self._traj_starts = []  # first observation in each traj
 
-    def add(self, transition: Transition):
+    def add(self, transition: Transition, is_first=False):
         if len(self._buffer) >= self._max_size:
             print(f"buffer full with size {len(self._buffer)}")
             print(f"skip adding obs {transition}")
+
+        if not is_first:
+            transition.prev = self._buffer[-1] if len(self._buffer) > 0 else None
+            if len(self._buffer) > 0:
+                self._buffer[-1].next = transition
+
         self._buffer.append(transition)
+
+        if transition.first:
+            self._traj_starts.append(transition)
 
     def __len__(self):
         return len(self._buffer)
@@ -79,13 +93,35 @@ class ReplayBuffer:
     ):
         return self._buffer[idx]
 
+    def clear(self):
+        self._buffer.clear()
+        self._traj_starts.clear()
+
     def traj_starts(self):
-        # returns iterator of trajectory starts?
         return self._traj_starts
-    
+
     def to_traj_list(self):
-        raise NotImplementedError()
+        trajs = []
+        for traj_start in self._traj_starts:
+            # iterate through the transitions till the end of the trajectory
+            traj = []
+            transition = traj_start
+            while not transition.done:
+                traj.append(transition.to_tuple())
+                transition = transition.next
+
+            trajs.append(traj)
+        return trajs
 
     @classmethod
-    def load_traj_list(self):  # this is traj_list --> buffer parser
-        raise NotImplementedError()
+    def load_traj_list(traj_list):  # this is traj_list --> buffer parser
+        buffer = ReplayBuffer()
+        buffer.append_traj_list(traj_list)
+        return buffer
+
+    def append_traj_list(self, traj_list):
+        for traj in traj_list:
+            for i, trans in enumerate(traj):
+                obs, action, reward = trans
+                transition = Transition(ObsWrapper.from_dict(obs), action, reward)
+                self.add(transition, i == 0)
